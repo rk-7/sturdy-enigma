b7131e28349652ab626d69190eeecf5b36c26b33
Rushikesh <30998401+rk-7@users.noreply.github.com>
2020-09-11 09:13:34 +0530

    Implemented gallery filter component

--- src/app/gallery-filter/gallery-filter.component.ts
+++ src/app/gallery-filter/gallery-filter.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { LaunchFilter } from '../models/launch-filter';
+import { range } from '../utils/range.generator';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-gallery-filter',
   templateUrl: './gallery-filter.component.html',
   styleUrls: ['./gallery-filter.component.scss']
 })
-export class GalleryFilterComponent implements OnInit {
-
+export class GalleryFilterComponent implements OnInit, OnDestroy {
+  @Input() initialLaunchYear: number;
+  @Output() filtersUpdated = new EventEmitter<LaunchFilter>();
+  public years: number[];
+  public filtersForm = new FormGroup({
+    limit: new FormControl(undefined),
+    launch_success: new FormControl(undefined),
+    land_success: new FormControl(undefined),
+    launch_year: new FormControl(undefined)
+  });
+  private subscriptions: Subscription;
   constructor() { }
-
-  ngOnInit(): void {
+  ngOnDestroy(): void {
+    if (this.subscriptions){
+      this.subscriptions.unsubscribe();
+    }
   }
 
+  ngOnInit(): void {
+    this.years = range(this.initialLaunchYear, new Date().getFullYear());
+    this.filtersForm.valueChanges
+    .subscribe(value => this.filtersUpdated.emit(this.getNonEmptyValues(value)));
+  }
+  private getNonEmptyValues(value: LaunchFilter): LaunchFilter {
+    console.log(value);
+    return Object.entries(value).filter(kv => !(kv[1] === null || kv[1] === undefined || kv[1] === ''))
+    .reduce((accum, [k, v]) => {
+      accum[k] = v;
+      return accum;
+    }, new LaunchFilter());
+  }
 }
