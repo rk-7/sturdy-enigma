33ff2fdb93188344bddb4fb294d776b657f27755
Rushikesh <30998401+rk-7@users.noreply.github.com>
2020-09-11 09:14:30 +0530

    Committing app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,38 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { LaunchesGalleryComponent } from './launches-gallery/launches-gallery.component';
 import { GalleryFilterComponent } from './gallery-filter/gallery-filter.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
 
+import { SuccessfulPipe } from './pipes/successful.pipe';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LaunchCardComponent } from './launch-card/launch-card.component';
 @NgModule({
   declarations: [
     AppComponent,
     NotFoundComponent,
     LaunchesGalleryComponent,
-    GalleryFilterComponent
+    GalleryFilterComponent,
+    SuccessfulPipe,
+    LaunchCardComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatButtonToggleModule,
+    MatDividerModule,
+    MatListModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
